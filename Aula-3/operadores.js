d40ec15ccfecd3822935017cdd85a0fe638aad25
Carlos <carlos.santos@advise.com.br>
2019-09-12 08:35:26 -0300

    Criando módulos

---
+++ Aula-3/operadores.js
@@ -0,0 +1,12 @@
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mult = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+module.exports = {
+    adicao: add,
+    subtracao: sub,
+    multiplicacao: mult,
+    divisao: div
+}
