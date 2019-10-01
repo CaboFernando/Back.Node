726051f293274b3d68a0e1c56b86209c0c164528
Carlos <fer.1972@hotmail.com>
2019-10-01 08:24:52 -0300

    Aula-7 Correções finais

--- Aula-7/mysql/server.js
+++ Aula-7/mysql/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const mysql = require('mysql');
-const db = require('db');
+const db = require('./db');
 
 const app = express();
 
@@ -10,21 +9,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-    const users = await db.query('SELECT * FROM user')
-    res.jsonp(users)
+    try {
+        const users = await db.query('SELECT * from user');
+        res.jsonp(users);
+    }
+    catch (error) {
+        res.status(500).jsonp(error);
+    }
 });
 
 app.post('/user', async (req, res) => {
+
     const nome = req.body.nome;
     const email = req.body.email;
-    
-    const sqlQuery = `INSERT INTO user(nome, email) VALUES('${nome}','${email}')`;
+
+    const sqlQuery = `
+        INSERT INTO user(nome, email) 
+        VALUES ('${nome}','${email}')`;
 
     try {
         const user = await db.query(sqlQuery);
         res.jsonp(user);
     }
-    catch {
+    catch (error) {
         res.status(500).jsonp(error);
     }
 
@@ -35,16 +42,26 @@ app.put('/user/:id', async (req, res) => {
     const nome = req.body.nome;
     const email = req.body.email;
 
-    const sqlQuery = `UPDATE SET nome = '${nome}', email = '${email}' WHERE id = '${id}`;
+    const sqlQuery = `
+        UPDATE user 
+            set 
+                nome = '${nome}', 
+                email = '${email}'
+            WHERE 
+                id = ${id}
+    `;
 
     try {
         const user = await db.query(sqlQuery);
         res.jsonp(user);
     }
-    catch {
+    catch (error) {
         res.status(500).jsonp(error);
     }
 
 });
 
 app.listen(80);
+
+
+
